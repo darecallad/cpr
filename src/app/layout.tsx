df498605b58c86587dc83fda1ff67f7fb0119239
johnson <ashane199080706@hotmail.com.tw>
2025-11-17 15:19:13 -0800

    Add Google Search Console verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -74,6 +74,9 @@ export const metadata: Metadata = {
       "max-snippet": -1,
     },
   },
+  verification: {
+    google: "HPJApzRqyvJbh2G3VxZmmnRoWnKuFXLkImvKxGK8z-c",
+  },
   icons: {
     icon: "/favicon.svg",
   },
