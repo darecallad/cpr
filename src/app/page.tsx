93692df60bf4d9917d0cf21f73f017a3535e1dd1
johnson <ashane199080706@hotmail.com.tw>
2025-12-10 12:24:51 -0800

    optimize home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,25 @@
 "use client";
 
 import { HeroSection } from "@/components/HeroSection";
+import { StatsStrip } from "@/components/StatsStrip";
 import { WhyWaymakerSection } from "@/components/WhyWaymakerSection";
+import { DaycareValuePropSection } from "@/components/DaycareValuePropSection";
 import { DaycareOpportunitySection } from "@/components/DaycareOpportunitySection";
 import { OurCoursesSection } from "@/components/OurCoursesSection";
 import { ProcessSection } from "@/components/ProcessSection";
 import { TestimonialsSection } from "@/components/TestimonialsSection";
 import { PartnersSection } from "@/components/PartnersSection";
 import { SecondaryCTASection } from "@/components/SecondaryCTASection";
+
 export default function Page() {
   return (
     <main className="flex flex-col">
       <HeroSection />
+      <StatsStrip />
       <WhyWaymakerSection />
-      <OurCoursesSection />
+      <DaycareValuePropSection />
       <DaycareOpportunitySection />
+      <OurCoursesSection />
       <ProcessSection />
       <TestimonialsSection />
       <PartnersSection />
